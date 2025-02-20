1d2c06093e748122e61675eee61b25128b05478d
Connor Switala <swita014@umn.edu>
2025-02-20 11:17:19 -0600

    Updated TESTING

--- TESTING/TESTING.cpp
+++ TESTING/TESTING.cpp
@@ -11,15 +11,168 @@
 #include <omp.h> 
 
 
-using namespace std;
+using namespace std; 
+
+Matrix outerProduct(const Vector& a, const Vector& b) {
+    Matrix result = {}; // Initialize with zeros
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i][j] = a[i] * b[j];
+        }
+    }
+    return result;
+}
+
+Vector Fluxes(const Vector& Vi, const Vector& Vii) {
+
+	Vector Ui = primtoCons(Vi); 
+	Vector Uii = primtoCons(Vii); 
+	int nx = 1, ny = 0;
+	double R = 287;
+	double cv = R / (gamma - 1);
+
+	double g = 5.72; 
+	double dp = fabs(Vii[3] - Vi[3]) / min(Vii[3], Vi[3]);
+	double weight = 1 - 0.5 * (1 / ((g * dp) * (g * dp) + 1)); 
+	Vector V = weight * Vi + (1 - weight) * Vii; 
+	Vector U = primtoCons(V);  
+
+	double rho = V[0], u = V[1], v = V[2], p = V[3]; 
+
+	double uprime = u * nx + v * ny;
+	double a = sqrt(gamma * p / rho);  
+
+	double pe = (gamma - 1);
+	double pp = (gamma - 1) * (-0.5 * (u * u + v * v));
+	double h0 = (U[3] + p ) / rho;
+
+	double l_car_plus = 0.5 * (uprime + a + fabs(uprime + a));
+	double l_tild_plus = 0.5 * (uprime + a - uprime - a); 
+	double l_plus = uprime; 
+
+	Vector V1_PLUS = { l_car_plus / (a * a),
+		(u * l_car_plus + a * nx * l_tild_plus) / (a * a),
+		(v * l_car_plus + a * ny * l_tild_plus) / (a * a), 
+		(h0 * l_car_plus + a * uprime * l_tild_plus) / (a * a) }; 
+
+	Vector V2_PLUS = { l_tild_plus / a,
+		u * l_tild_plus / a + nx * l_car_plus,
+		v* l_tild_plus / a + ny * l_car_plus,
+		h0* l_tild_plus / a + uprime * l_car_plus };  
+
+	Vector m = { pp, -u * pe, -v * pe, pe }; 
+	Vector n = { -uprime, nx, ny, 0 };
+
+	Matrix A_Plus = outerProduct(V1_PLUS, m) + outerProduct(V2_PLUS, n) + l_plus * identity();
+
+
+	double l_car_minus = 0.5 * (uprime - a + fabs(uprime - a)); 
+	double l_tild_minus = 0.5 * (uprime - a - uprime - a); 
+	double l_minus = uprime; 
+
+
+	Vector V1_MINUS = { l_car_minus / (a * a),
+	(u * l_car_minus + a * nx * l_tild_minus) / (a * a),
+	(v * l_car_minus + a * ny * l_tild_minus) / (a * a),
+	(h0 * l_car_minus + a * uprime * l_tild_minus) / (a * a) };
+
+	Vector V2_MINUS = { l_tild_minus / a,
+	u * l_tild_minus / a + nx * l_car_minus,
+	v * l_tild_minus / a + ny * l_car_minus,
+	h0 * l_tild_minus / a + uprime * l_car_minus };
+
+	Matrix A_Minus = outerProduct(V1_MINUS, m) + outerProduct(V2_MINUS, n) + l_minus * identity(); 
+
+	return A_Plus * Ui + A_Minus * Uii; 
+}
+
+
+//Vector Fluxes(const Vector& Vi, const Vector& Vii) {
+//
+//	Vector Ui = primtoCons(Vi);
+//	Vector Uii = primtoCons(Vii);
+//
+//	double g = 5.72;
+//	double dp = fabs(Vii[3] - Vi[3]) / min(Vii[3], Vi[3]);
+//	double weight = 1 - 0.5 * (1 / ((g * dp) * (g * dp) + 1));
+//	Vector V = weight * Vi + (1 - weight) * Vii;
+//	Vector U = primtoCons(V);
+//
+//
+//	double rho = V[0], u = V[1], v = V[2], p = V[3];
+//	double R = 287;
+//	double cv = R / (gamma - 1);
+//
+//	int nx = 1, ny = 0;
+//	double uprime = u * nx + v * ny;
+//	double a = sqrt(gamma * p / rho);
+//
+//	double l1 = 0.5 * (uprime - a + fabs(uprime - a));
+//	double l2 = 0.5 * (uprime + fabs(uprime));
+//	double l3 = 0.5 * (uprime + fabs(uprime));
+//	double l4 = 0.5 * (uprime + a + fabs(uprime + a));
+//
+//	double l_car_plus = 0.5 * (l4 + l1 - 2 * l2);
+//	double l_tild_plus = 0.5 * (l4 - l1);
+//	double l_plus = l2;
+//
+//	l1 = 0.5 * (uprime - a - fabs(uprime - a));
+//	l2 = 0.5 * (uprime - fabs(uprime));
+//	l3 = 0.5 * (uprime - fabs(uprime));
+//	l4 = 0.5 * (uprime + a - fabs(uprime + a));
+//
+//	double l_car_minus = 0.5 * (l4 + l1 - 2 * l2);
+//	double l_tild_minus = 0.5 * (l4 - l1);
+//	double l_minus = l2;
+//
+//	double pe = R / cv;
+//	double pp = R / (2 * cv) * (u * u + v * v);
+//	double h0 = (U[3] + V[3]) / rho;
+//
+//	Vector V1_PLUS = { l_car_plus / (a * a),
+//		(u * l_car_plus + a * nx * l_tild_plus) / (a * a),
+//		(v * l_car_plus + a * ny * l_tild_plus) / (a * a),
+//		(h0 * l_car_plus + a * uprime * l_tild_plus) / (a * a) };
+//
+//	Vector V1_MINUS = { l_car_minus / (a * a),
+//	(u * l_car_minus + a * nx * l_tild_minus) / (a * a),
+//	(v * l_car_minus + a * ny * l_tild_minus) / (a * a),
+//	(h0 * l_car_minus + a * uprime * l_tild_minus) / (a * a) };
+//
+//	Vector V2_PLUS = { l_tild_plus / a,
+//		u * l_tild_plus / a + nx * l_car_plus,
+//		v * l_tild_plus / a + ny * l_car_plus,
+//		h0 * l_tild_plus / a + uprime * l_car_plus };
+//
+//	Vector V2_MINUS = { l_tild_minus / a,
+//		u * l_tild_minus / a + nx * l_car_minus,
+//		v * l_tild_minus / a + ny * l_car_minus,
+//		h0 * l_tild_minus / a + uprime * l_car_minus };
+//
+//
+//	Vector m = { pp, -u * pe, -v * pe, pe };
+//	Vector n = { -uprime, nx, ny, 0 };
+//
+//	Matrix A_Plus = outerProduct(V1_PLUS, m) + outerProduct(V2_PLUS, n) + l_plus * identity();
+//	Matrix A_Minus = outerProduct(V1_MINUS, m) + outerProduct(V2_MINUS, n) + l_minus * identity();
+//
+//	return A_Plus * Ui + A_Minus * Uii;
+//}
+//
 
 
 
 int main() {
 
-	CylinderGrid grid(50, 25, 0.1, 0.3, 0.45, 0.001, pi / 2, 3 * pi / 2);
-
-    cout << grid.jArea(45, 24) << endl;
+	Vector Vi = { 0.266, 0.927, 0.0, 0.303 };
+	Vector Vii = { 0.125, 0.0, 0.0, 0.1 }; 
+	Vector F; 
+	auto start = TIME; 
+	F = Fluxes(Vi, Vii);	
+	auto end = TIME;
+	DURATION duration = end - start;
+	cout << "Fluxes took: " << duration.count() << " seconds." << endl;
+	displayVector(F);
 
     return 0;
 }
